fb981dc0bc26fcb178bd96b6dcece5ef3317bfbe
MuZhiFei <34238232+MuZhiFei@users.noreply.github.com>
2021-04-23 18:20:20 +0800

    Add files via upload

---
+++ GetResponse.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * GetResponse.php
+ *
+ * @author libojian <bojian.li@foxmail.com>
+ * @since 2021/4/23 17:58
+ * @version 0.1
+ */
+
+/**
+ * 通用文件上传类
+ * @author  guanguans <yzmguanguan@gmail.com>
+ */
+namespace meiqia;  // 注意命名空间与 composer.json 中的一致
+
+require_once '../vendor/autoload.php';
+
+echo 'hello, world!';
